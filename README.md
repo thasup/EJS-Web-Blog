c5c46696a609170867aa151259ecf99fa8b72b75
thasup <thanachonfirst@hotmail.com>
2021-12-11 04:18:56 +0700

    Add comments & readme
    prepared project to deploy on Heroku

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
+<!-- Add banner here -->
+
 # EJS-Web-Blog
- 
+<!-- Describe your project in brief -->
+A web blog that uses **EJS** to create a template and run javascript on HTML. It contains a function to post a new article and link all pages with Express's route method.
+
+[EJS Web Blog](https://thasup-ejs-web-blog.herokuapp.com/)
+
+# Table of contents
+
+- [EJS Web Blog](#ejs-web-blog)
+- [Table of contents](#table-of-contents)
+- [Features](#features)
+- [Install](#install)
+- [Languages](#languages)
+- [Technology](#technology)
+
+# Features
+[(Back to top)](#table-of-contents)
+
+- Post new article on blog
+- Linked all pages to freely navigate on the website
+
+# Install
+[(Back to top)](#table-of-contents)
+
+1. clone this git on your computer
+2. install [Node.js](https://nodejs.org/en/) 
+3. run command `npm install` on terminal
+4. run command `nodemon app` on terminal
+5. go to `http://localhost:3000/`
+6. happy!
+
+# Languages
+[(Back to top)](#table-of-contents)
+
+- HTML
+- CSS
+- Javascript
+- EJS
+
+# Technology
+[(Back to top)](#table-of-contents)
+
+- HTML
+- CSS
+- Javascript
+- Bootstrap
+- EJS
+- Node.js
+- Express.js
+- Heroku
--- app.js
+++ app.js
@@ -1,21 +1,32 @@
+// Require modules
 const express = require("express");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-// Load the full build.
-const _ = require('lodash');
-
+// Start up an instance of app
 const app = express();
 
-app.set('view engine', 'ejs');
+// Setup EJS
+app.set("view engine", "ejs");
 
+// Add middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+// Defines the port number
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+// Defined global variables
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum constius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const postData = [];
 
+// GET route for root
 app.get("/", (req, res) => {
   res.render("home", {
     ejsHomeContent: homeStartingContent,
@@ -23,22 +34,26 @@ app.get("/", (req, res) => {
   });
 });
 
+// GET route for about page
 app.get("/about", (req, res) => {
   res.render("about", {
     ejsAboutContent: aboutContent
   });
 });
 
+// GET route for contact page
 app.get("/contact", (req, res) => {
   res.render("contact", {
     ejsContactContent: contactContent
   });
 });
 
+// GET route for compose page
 app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+// POST route for retrieve input data from compose page
 app.post("/compose", (req, res) => {
   let post = {
     title: req.body.postTitle,
@@ -52,6 +67,7 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
+// GET route for specific pose page
 app.get("/posts/:postName", (req, res) => {
   const requestedTitle = _.lowerCase(req.params.postName);
 
@@ -67,10 +83,4 @@ app.get("/posts/:postName", (req, res) => {
       console.log("Not a Match");
     }
   });
-});
-
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
 });
