0abf67ecf5af2f4badb81f4b1863a29e6bb01a01
thasup <thanachonfirst@hotmail.com>
2021-12-10 23:31:47 +0700

    Edit start files

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-//jshint esversion:6
-
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -12,9 +9,16 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home", {
+    ejsHomeContent: homeStartingContent,
+    ejsAboutContent: aboutContent,
+    ejsContactContent: contactContent
+  });
+});
 
 
 
@@ -25,9 +29,8 @@ app.use(express.static("public"));
 
 
 
+const port = 3000;
 
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
